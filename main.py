53854255c66444f5babf5de6d973c8e73b6e16fa
X - MrG3P5 <57594747+MrG3P5@users.noreply.github.com>
2022-06-20 03:42:19 +0700

    bened klo make thread owakow
    bened klo make thread owakow

--- main.py
+++ main.py
@@ -4,7 +4,6 @@
 import pyfiglet
 import requests
 import os
-from threading import Thread
 from colorama import Fore, init
 from random import randint
 
@@ -13,9 +12,6 @@ red = Fore.LIGHTRED_EX
 green = Fore.LIGHTGREEN_EX
 yellow = Fore.LIGHTYELLOW_EX
 white = Fore.WHITE
-ATTEMP_R1 = 0
-ATTEMP_R2 = 0
-ATTEMP_R3 = 0
 init(autoreset=True)
 
 def banner(str):
@@ -25,47 +21,27 @@ def banner(str):
     print(f"\t\t\t{red}[ {white}Created By X - MrG3P5 {red}]")
     print(f"\t\t{red}[ {white}This tools for auto win in game stumble{red} ]\n")
 
-def start(token):
-    global ATTEMP_R1, ATTEMP_R2, ATTEMP_R3
+def start():
     banner("X - Stumble")
+    input_auth = input(f"{red}[{white}?{red}] {white}Enter your auth token : ")
 
     while True:
         try:
             random_round = randint(1, 3)
-            req_game = requests.get(f"http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/{random_round}", headers={
-                "authorization": token
-            }).json()
+            req_game, status = requests.get(f"http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/{random_round}", headers={
+                "authorization": input_auth
+            })
+            print(req_game)
+            print(status)
             if "BANNED" in str(req_game):
                 print(f"{red}[{yellow}*{red}] {white}Account Got Banned")
+                break
             elif "SERVER_ERROR" in str(req_game):
                 continue
             elif "User" in str(req_game):
                 print(f"{red}[{white}*{red}] {white}Nickname: {green}{req_game['User']['Username']} {white}| Country: {green}{req_game['User']['Country']} {white}| Trophy: {green}{req_game['User']['SkillRating']} {white}| Crown: {green}{req_game['User']['Crowns']}")
         except:
-            if random_round == 1:
-                if ATTEMP_R1 == 5 and ATTEMP_R2 == 5 and ATTEMP_R3 == 5:
-                    print(f"{red}[{yellow}*{red}] {white}Wrong cookie or Expired cookie!")
-                    break
-                else:
-                    ATTEMP_R1 += 1
-                    continue
-            elif random_round == 2:
-                if ATTEMP_R2 == 5 and ATTEMP_R2 == 5 and ATTEMP_R3 == 5:
-                    print(f"{red}[{yellow}*{red}] {white}Wrong cookie or Expired cookie!")
-                    break
-                else:
-                    ATTEMP_R2 += 1
-                    continue
-            elif random_round == 3:
-                if ATTEMP_R3 == 5 and ATTEMP_R2 == 5 and ATTEMP_R3 == 5:
-                    print(f"{red}[{yellow}*{red}] {white}Wrong cookie or Expired cookie!")
-                    break
-                else:
-                    ATTEMP_R3 += 1
-                    continue
+            continue
 
 if __name__=="__main__":
-    banner("X - Stumble")
-    input_auth = input(f"{red}[{white}?{red}] {white}Enter your auth token : ")
-    t = Thread(target=start, args=(input_auth,))
-    t.start()
+    start()
