1af6b8356a6983fb63a78025f0c3db1c2f50458d
X - MrG3P5 <57594747+MrG3P5@users.noreply.github.com>
2022-06-23 19:09:15 +0700

    add delay
    add delay

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import pyfiglet
 import requests
 import os
+from time import sleep
 from datetime import datetime
 from pytz import timezone
 from colorama import Fore, init
@@ -31,9 +32,11 @@ def start():
     banner("X - Stumble")
     input_auth = input(f"{red}[{white}?{red}] {white}Enter your auth token : ")
     round_input = input(f"{red}[{white}?{red}] {white}Enter round (1, 2, 3) : ")
+    delay_input = input(f"{red}[{white}?{red}] {white}Enter Delay (ex: 1 = 1sec) : ")
 
     while True:
         try:
+            sleep(int(delay_input))
             req_game = requests.get(f"http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/{round_input}", headers={
                 "authorization": input_auth
             }).json()
